f55465fc7d9a8cbb54d324be2e0d077239114e2b
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-21 09:27:55 +0100

    [added] Added files from blackboard.

---
+++ LottoPlayer/LottoPlayer.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lotto
+{
+    /// <summary>
+    /// Represent a Lotto Player
+    /// </summary>
+    /// <author> Steffen Skov</author>
+    class LottoPlayer
+    {
+        DanskeSpil ds;
+        public LottoPlayer(DanskeSpil ds)
+        {
+            this.ds = ds;
+            // Subscribe the Lotto drawing by DanskeSpil 
+        }
+          
+        private int[] myNum = { 1, 3, 5, 7, 9, 11, 13 };
+
+        /// <summary>
+        /// My Lotto nummers
+        /// 7 numbers between 1 and 36
+        /// </summary>
+        public int[] MyNum
+        {
+            get { return myNum; }
+            set 
+            { 
+                if (LegalNumbers(value) == true)
+                    myNum = value; 
+                else
+                    throw new Exception("Wrong numbers");
+            }
+        }
+
+        /// <summary>
+        /// Check for legal Lotto numbers
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private bool LegalNumbers(int[] n)
+        {
+            bool legal = true;
+            if (n.Length == 7)
+            {
+                for (int i = 0; i < 7; i++)
+                    if (n[i] > 37 || n[i] < 0)
+                        legal = false;
+            }
+            else
+                legal = false;
+            return legal;
+        }
+
+        /// <summary>
+        /// Event handler method
+        /// </summary>
+        /// <param name="n">Lotto numbers</param>
+        public void CheckMyNum(int[] n)
+        {
+            bool luck = true;
+            for (int i = 0; i < 7; i++)
+            {
+                if (n[i] != myNum[i]) luck = false;
+            }
+            if (luck == true)
+                Console.WriteLine("Now I am Millionaire");
+        }
+    }
+
+}
--- LottoPlayer/Program.cs
+++ LottoPlayer/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace LottoPlayer
+namespace Lotto
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    /// <summary>
+    /// Simulate Lotto game provider "Danske Spil"
+    /// Replace comment with your code
+    /// </summary>
+    /// <author> Steffen Skov</author>
+    class DanskeSpil
+    {
+        // define a DELEGAT so it can sende Lotto numbers to subscribes
+		
+        // Create an EVENT or a delegate instance
+
+        static void Main(string[] args)
+        {
+            DanskeSpil ds = new DanskeSpil();
+            LottoPlayer pl = new LottoPlayer(ds);
+            // Try to create more player with different numbers
+            // When number is set remember to catch exception
+            ds.Draw();
+        }
+
+        /// <summary>
+        /// This method simulate drawing at DanskeSpil
+        /// </summary>
+        public void Draw()
+        {
+            Random n = new Random(DateTime.Now.Millisecond);
+            int [] num = new int[7];
+            for (;;)
+            {
+                for (int i = 0; i <7; i++) num[i] = n.Next(1,36);
+                // To increase the probability so only one of numbers will change
+                Thread.Sleep(6000);
+                Console.WriteLine(num[0] + " " + num[1] + " " + num[2] + " " + num[3] + " " + num[4] + " " + num[5] + "\n");
+                // FIRE the event
+            }
+        }
+    }
+
 }
